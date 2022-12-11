826e86d1072543b3ad3403992470dc1845858392
meraf00 <meraf000@gmail.com>
2022-12-11 03:01:33 +0300

    Prepared documentation directory

---
+++ server/docs/doc.py
@@ -0,0 +1,10 @@
+"""Gets and prints the spreadsheet's header columns
+
+Args:
+    file_loc (str): The file location of the spreadsheet
+    print_cols (bool): A flag used to print the columns to the console
+        (default is False)
+
+Returns:
+    list: a list of strings representing the header columns
+"""
